be3701ab3e3fde6df18cd380ecdbced226ad66ec
Stéphane HULARD <s.hulard@chstudio.fr>
2015-08-10 16:46:47 +0200

    Cleanup invalid licence headers
    Signed-off-by: Stéphane HULARD <s.hulard@chstudio.fr>

--- src/Detector.php
+++ src/Detector.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * This file is part of the beebot package.
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @copyright Bee4 2013
- * @author  Stephane HULARD <s.hulard@chstudio.fr>
- * @package Bee4\UserAgent\Classifier
- */
 
 namespace Bee4\UserAgent\Classifier;
 
--- test/units/Bots/DiscoBotTest.php
+++ test/units/Bots/DiscoBotTest.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * This file is part of the beebot package.
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @copyright Bee4 2013
- * @author    Stephane HULARD <s.hulard@chstudio.fr>
- * @author    Ivo GEORGIEV <ivokgeorgiev@gmail.com>
- * @package   Test\Bee4\UserAgent\Classifier\Bots
- */
+
 namespace Test\Bee4\UserAgent\Classifier\Bots;
 
 use Bee4\UserAgent\Classifier\Bots\DiscoBot;
